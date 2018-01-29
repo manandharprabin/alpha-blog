8051f107ce550f52f75dcc88420394995f5ad3d4
Prabin Manandhar <manandharprabin@gmail.com>
2018-01-29 14:58:12 +0100

    SignUp and Edit User added.

--- Gemfile
+++ Gemfile
@@ -10,6 +10,7 @@ end
 gem 'bootstrap-sass', '~> 3.3.7'
 gem 'jquery-rails'
 gem 'rails', '~> 5.1.4'
+gem 'bcrypt', '~> 3.1.7' # to decrypt password 
 
 # Use Puma as the app server
 gem 'puma', '~> 3.7'
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,9 @@
 class User < ApplicationRecord
 	has_many :articles
 	before_save {self.email = email.downcase}
-	validates :user_name, presence: true, uniqueness: {case_sensitive: false}, 	length: {minimum: 3, maximum: 25}
+	validates :user_name, presence: true, uniqueness: {case_sensitive: false},length: {minimum: 3, maximum: 25}
 	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-	validates :email, presence: true, uniqueness: {case_sensitive: false}, 	length: {minimum: 3, maximum: 25}, 
+	validates :email, presence: true, uniqueness: {case_sensitive: false}, length: {minimum: 3, maximum: 25}, 
 			  format: {with: VALID_EMAIL_REGEX}
+	has_secure_password
 end
