bc98f205187cb5b76794ff189062734c6b9e5808
Prabin Manandhar <manandharprabin@gmail.com>
2018-02-01 11:28:52 +0100

    User authentication completed and articles created belongs to current user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
 	before_action :set_user, only: [:edit, :update, :show]
+	before_action :require_same_user, only: [:edit, :update]
 	def index
 		@users = User.paginate(page: params[:page], per_page: 5)
 	end
@@ -10,7 +11,7 @@ class UsersController < ApplicationController
 
 	def create
 		#debugger
-		@user = User.new (user_params)
+		@user = User.new(user_params)
 		if @user.save
 			flash[:success] = "Welcome to the alpha blog #{@user.user_name}"
 			redirect_to articles_path
@@ -45,5 +46,12 @@ class UsersController < ApplicationController
 
 		def set_user
 			@user = User.find(params[:id])
+		end
+
+		def require_same_user
+			if current_user != @user
+				flash[:danger] = "You have no authority to edit this user."
+				redirect_to root_path
+			end
 		end
 end
